c43ccea541c8471063fac9240cc9812e9b7918ec
skywalker512 <houzhenhong@hotmail.com>
2017-08-16 10:58:22 +0800

    update fonts cdn

--- src/Http/WebApp/WebAppView.php
+++ src/Http/WebApp/WebAppView.php
@@ -196,7 +196,7 @@ class WebAppView
         $this->userSerializer = $userSerializer;
         $this->app = $app;
 
-        $this->addHeadString('<link rel="stylesheet" href="//fonts.css.network/css?family=Open+Sans:400italic,700italic,400,700,600">', 'font');
+        $this->addHeadString('<link rel="stylesheet" href="//fonts.cat.net/css?family=Open+Sans:400italic,700italic,400,700,600">', 'font');
 
         $this->js = $this->assets->getJs();
         $this->css = $this->assets->getCss();
